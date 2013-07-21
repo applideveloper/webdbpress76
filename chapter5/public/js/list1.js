b4d6e309f7aad5a69b7eea3c03c1fa1bde4a10ce
hmsk <k.hamasaki@gmail.com>
2013-07-22 01:16:12 +0900

    remove surplus $ on JS

--- chapter5/public/js/list1.js
+++ chapter5/public/js/list1.js
@@ -1,10 +1,10 @@
 WebPay.setPublishableKey(
     'test_public_62W08o3YN0mj5T88953A34mZ');
 var webpayResponseHandler = function(status, response) {
-  var $form = $("#payment-form");
+  var form = $("#payment-form");
   if (response.error) {
     // 必要に応じてエラー処理を入れてください
-    $form.find('button').prop('disabled', false);
+    form.find('button').prop('disabled', false);
     console.log(response);
   } else {
     // 伝送させたくない情報をフォームから削除する
@@ -13,23 +13,23 @@ var webpayResponseHandler = function(status, response) {
     $(".card-expiry-year").removeAttr("name");
 
     var token = response.id;
-    $form.append(
+    form.append(
       "<input type='hidden' name='token' value='" + token + "'/>"
     );
-    $form.get(0).submit();
+    form.get(0).submit();
   }
 };
 
 jQuery(function($) {
   $('#payment-form').submit(function(e) {
-    var $form = $(this);
-    $form.find('button').prop('disabled', true);
+    var form = $(this);
+    form.find('button').prop('disabled', true);
     WebPay.createToken({
-      number: $form.find(".card-number").val(),
-      name: $form.find(".card-name").val(),
-      cvc: $form.find(".card-cvc").val(),
-      exp_month: $form.find(".expiry-month").val(),
-      exp_year: "20" + $form.find(".expiry-year").val()
+      number: form.find(".card-number").val(),
+      name: form.find(".card-name").val(),
+      cvc: form.find(".card-cvc").val(),
+      exp_month: form.find(".expiry-month").val(),
+      exp_year: "20" + form.find(".expiry-year").val()
     }, webpayResponseHandler);
     e.preventDefault();
   });
